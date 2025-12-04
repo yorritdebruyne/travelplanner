7fdc8c283b78704126559672f1439a52e15def92
Yorrit <yorritdebruyne@gmail.com>
2025-12-04 15:59:18 +0100

    Created test code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import Controller.TripController;
+import Manager.TripManager;
+import View.TripListView;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Test  Github");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Create TripListView singleton instance
+        TripListView tripListView = new TripListView();
+
+        // Register TripListView as observer of TripManager singleton instance
+        TripManager.getInstance().registerObserver(tripListView);
+
+        // Create TripManager singleton instance
+        TripController tripController = new TripController();
+
+        // Testing adding trip to Controller
+        tripController.createTrip(
+                "TripWithTheBoys",
+                "Barcelona",
+                "Weekend getaway",
+                "01/05/2026",
+                "05/05/2026"
+        );
+
+        // Add another trip
+        tripController.createTrip(
+                "FamilyHoliday",
+                "Rome",
+                "Summer trip",
+                "10/07/2026",
+                "20/07/2026"
+        );
+
+        // Delete a trip
+        tripController.deleteTrip("TripWithTheBoys");
     }
 }
