9ae9cb45e17540870271603cf950d9da4d66a9eb
Yorrit <yorritdebruyne@gmail.com>
2025-12-11 13:56:56 +0100

    Changed filename

--- src/Command/UpdateItineraryItemCommand.java
+++ src/Command/UpdateItineraryItemCommand.java
@@ -3,12 +3,12 @@ package Command;
 import Manager.TripManager;
 import Model.ItineraryItem;
 import Model.Trip;
-public class updateItineraryItemCommand implements Command {
+public class UpdateItineraryItemCommand implements Command {
     private final String tripTitle;
     private final ItineraryItem oldItem, newItem;
     private final TripManager tripManager;
 
-    public updateItineraryItemCommand(String tripTitle, ItineraryItem oldItem, ItineraryItem newItem, TripManager tripManager) {
+    public UpdateItineraryItemCommand(String tripTitle, ItineraryItem oldItem, ItineraryItem newItem, TripManager tripManager) {
         this.tripTitle = tripTitle;
         this.oldItem = oldItem;
         this.newItem = newItem;
